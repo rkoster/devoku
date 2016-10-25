43a23c87f0841dbcb2fd8ef19fafcffb8928fd4e
Raymond Reggers <raymond@Raymonds-MacBook-Pro.local>
2016-10-25 15:42:29 +0200

    fix long running

--- include/utils.bash
+++ include/utils.bash
@@ -18,12 +18,20 @@ long-running()
     local delay=0.25
     local spin=('\xe2\xa3\xbe' '\xe2\xa3\xb7' '\xe2\xa3\xaf' '\xe2\xa3\x9f' '\xe2\xa1\xbf' '\xe2\xa2\xbf' '\xe2\xa3\xbb' '\xe2\xa3\xbd')
 		echo -ne "$2  "
-    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+
+		# If this script is killed, kill the process
+		trap "kill $pid 2> /dev/null" EXIT
+
+    while kill -0 $pid 2> /dev/null; do
 			for i in "${spin[@]}"
 		  do
         echo -ne "\b\e[92m$i\e[39m"
         sleep $delay
 		  done
     done
+
+		# Disable the trap on a normal exit.
+		trap - EXIT
+		
 		echo -e "\b\e[92m[DONE]\e[39m"
 }
