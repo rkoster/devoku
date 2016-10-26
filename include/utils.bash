0e5de78ed990fc7b1e0d2e9b77f5c8aeee6dbb36
Raymond Reggers <raymond@adaptiv.nl>
2016-10-26 15:48:19 +0200

    fix spinner

--- include/utils.bash
+++ include/utils.bash
@@ -17,7 +17,7 @@ long-running()
     local pid=$1
     local delay=0.25
     local spin=('\xe2\xa3\xbe' '\xe2\xa3\xb7' '\xe2\xa3\xaf' '\xe2\xa3\x9f' '\xe2\xa1\xbf' '\xe2\xa2\xbf' '\xe2\xa3\xbb' '\xe2\xa3\xbd')
-		echo -ne "$2  "
+		printf "$2  "
 
 		# If this script is killed, kill the process
 		trap "kill $pid 2> /dev/null" EXIT
@@ -25,7 +25,7 @@ long-running()
     while kill -0 $pid 2> /dev/null; do
 			for i in "${spin[@]}"
 		  do
-        echo -e "\b\e[92m$i\e[39m"
+        printf "\b\e[92m$i\e[39m"
         sleep $delay
 		  done
     done
@@ -33,5 +33,5 @@ long-running()
 		# Disable the trap on a normal exit.
 		trap - EXIT
 
-		echo -e "\b\e[92m[DONE]\e[39m"
+		printf "\b\e[92m[DONE]\e[39m\n"
 }
